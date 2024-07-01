676dbc8b831b40a7c30b2178c7d9f9c8d543edd2
Gayatri <gayatriloungani2021@gmail.com>
2024-07-02 01:29:06 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+
+import {BrowserRouter , Routes , Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Profile from './Pages/Profile'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import Header from './components/Header'
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/profile" element={<Profile/>} />
+      <Route path="/signIn" element={<SignIn/>} />
+      <Route path="/signUp" element={<SignUp/>} />
+      </Routes>
+      </BrowserRouter>
+  )
+}
