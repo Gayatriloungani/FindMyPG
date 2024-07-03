475a2151f1404a64cec37142c6c71cb5d1e6f629
Gayatri <gayatriloungani2021@gmail.com>
2024-07-03 21:09:50 +0530

    api route created

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user_route.js'
+
 dotenv.config();
 
-
+//databse connected
 mongoose.connect(process.env.mongo_db_url).then(() => {
     console.log("connected to db");
 }).catch((error) => {
@@ -14,5 +16,7 @@ mongoose.connect(process.env.mongo_db_url).then(() => {
 const app = express();
 
 app.listen(3000 , () => {
- console.log("server running");
+ console.log("server running on port - 3000");
 })
+
+app.use('/api/user', userRouter);
