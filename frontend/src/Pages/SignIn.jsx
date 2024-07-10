2a6b0cb9abdb563fbb1468729010945fa7511f4b
Gayatri <gayatriloungani2021@gmail.com>
2024-07-10 22:01:07 +0530

    redux in sign in

--- frontend/src/Pages/SignIn.jsx
+++ frontend/src/Pages/SignIn.jsx
@@ -1,13 +1,18 @@
 import {Link, useNavigate} from 'react-router-dom'
 import { useState } from 'react';
 import {toast} from 'react-toastify';
-
+import {useDispatch , useSelector} from 'react-redux';
+import {signInStart , signInSuccess , signInFailure} from '../redux/user/userSlice.js'
 function SignIn() {
 
   const [formData , setFormData] = useState({});
-  const [loading , setLoading] = useState(false);
-  const [error , setError] = useState(null);
+
+  // const [loading , setLoading] = useState(false);
+  // const [error , setError] = useState(null);
+  
+  const {loading , error} = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   
   const handleChange = (e) => {
         setFormData({
@@ -20,7 +25,8 @@ function SignIn() {
      e.preventDefault();
 
      try{
-      setLoading(true);
+      // setLoading(true);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin' , 
        {
          method:'POST',
@@ -32,20 +38,19 @@ function SignIn() {
      const data = await res.json();
      console.log(data);
      if(data.success === false){
-       setError(data.message);
-       setLoading(false);
+      //  setError(data.message);
+      //  setLoading(false);
+      dispatch(signInFailure(data.message));
        toast.error("something went wrong");
        return;
      }
-     setLoading(false);
-     setError(null);
+     dispatch(signInSuccess(data));
      toast.success("User successfully sign in !! ");
      navigate('/')
       
      }catch(error){
-         setLoading(false);
-         setError(error.message);
-         toast.error("something went wrong");
+       dispatch(signInFailure(error.message));
+      toast.error("something went wrong");
      }
   }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { store ,persistor} from './redux/store.js';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <App />
     <ToastContainer/>
-  </React.StrictMode>,
+    </PersistGate>
+  </Provider>,
 )
