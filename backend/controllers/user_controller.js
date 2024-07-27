28133478f077312ebab3dce218f890c83ba0f45d
Gayatri <gayatriloungani2021@gmail.com>
2024-07-27 21:41:51 +0530

    complete delete  user functionality

--- backend/controllers/user_controller.js
+++ backend/controllers/user_controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user_model.js";
 import { errorHandler } from "../utils/error.js";
 import bcrypt from 'bcrypt';
+import Listing from '../models/listing_model.js';
 
 export const test = (req, res) => {
     res.json({
@@ -59,5 +60,18 @@ export const deleteUser = async(req,res,next) => {
         });
     } catch (error) {
         next(error);
+    }
+}
+
+export const getUserListings = async(req,res,next) => {
+    if(req.user.id === req.params.id){
+        try{
+           const listings = await Listing.find({userRef:req.params.id});
+           res.status(200).json(listings);
+        } catch(error){
+            next(error);
+        }
+    }else{
+        return next(errorHandler(401, 'You can only view your own listings'));
     }
 }
